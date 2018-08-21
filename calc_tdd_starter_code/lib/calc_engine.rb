e50c94688ddc7cd4edfd226645541e17c6dfdce4
“Mhaventhan” <mhaventhan@gmail.com>
2018-08-21 21:51:42 +0100

    ruby calculator

--- calc_tdd_starter_code/lib/calc_engine.rb
+++ calc_tdd_starter_code/lib/calc_engine.rb
@@ -1,34 +1,51 @@
 class CalcEngine
 
   def run_calculator
-    puts "------------"
-    puts "Running Calc -  Adding numbers"
-    puts add_two_numbers(2,4)
-    puts "------------"
-    puts "What is you name?"
-    user_input = gets.chomp
-    puts "Your name is #{user_input}"
-    puts "Add code here to finish calculator"
-    puts "------------"
-  end
+    puts "Select calculator type b) Basic, a) Advanced and i) BMI"
+    option = gets.chomp
+    puts "Enter your first number"
+    num1 = gets.chomp
+    puts "Enter your second number"
+    num2 = gets.chomp
 
-  def add_two_numbers(num1, num2)
-    num1 + num2
-  end
+    case option
 
-  def sub_two_numbers(num1,num2)
-    num1 - num2
-  end
-
-  def multiply_two_numbers(num1,num2)
-    num1 * num2
-  end
-
-  def divide_two_numbers(num1,num2)
-    num1 / num2
-  end
-
+    when 'b'
+      operator = gets.chomp
+      puts "select your operator: 1)add, 2) subtract, 3) multiply and 4) divide"
+    when '1'
+      def add_two_numbers(num1, num2)
+        num1 + num2
+      end
+    when '2'
+      def sub_two_numbers(num1,num2)
+        num1 - num2
+      end
+    when '3'
+      def multiply_two_numbers(num1,num2)
+        num1 * num2
+      end
+    when '4'
+      def divide_two_numbers(num1,num2)
+        num1 / num2
+      end
+    when 'a'
+      def sqrRoot(num1)
+        Math.sqrt(num1)
+      end
+      def power(num1,num2)
+        num1 ** num2
+      end
+    when 'i'
+      def bmiMetric (weight, height)
+        return weight / height * height
+      end
+      # def bmiImperial (weight, height)
+      #   total = (weight / height **2) * 703
+      #   return total.round
+      # end
+    end
+    end
 end
-
-# calc = CalcEngine.new()
-# calc.run_calculator
+calc = CalcEngine.new()
+calc.run_calculator
--- calc_tdd_starter_code/spec/calc_engine_spec.rb
+++ calc_tdd_starter_code/spec/calc_engine_spec.rb
@@ -10,4 +10,23 @@ describe "Testing the calc engine" do
   it "should add two numbers together" do
     expect(@calc.add_two_numbers(1,1)).to eq(2)
   end
+  it "should subtract two numbers" do
+    expect(@calc.sub_two_numbers(1,1)).to eq(0)
+  end
+  it "should multiply two numbers" do
+    expect(@calc.multiply_two_numbers(1,1)).to eq(1)
+  end
+  it "should divide two numbers" do
+    expect(@calc.divide_two_numbers(1,1)).to eq(1)
+  end
+  it "square root of the number" do
+    expect(@calc.sqrRoot(25)).to eq(5)
+  end
+  it "Should workout the power" do
+    expect(@calc.power(2,5)).to eq(32)
+  end
+
+  it "Should return BMI for metric" do
+    expect(@calc.bmiMetric(100,65)).to eq(65)
+  end
 end
